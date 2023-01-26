24276953cc65bbcb02d7a25720bd379a879368c6
Nikhil Jha <jhanikhil19@gmail.com>
2023-01-27 04:43:24 +0530

    Create using.py

---
+++ using.py
@@ -0,0 +1,15 @@
+#Python external and built in modules
+
+import random
+random_num = random.randint(0,10)
+print(random_num)
+
+num = random.random()
+print(num)
+
+lst = ["Nikhil","Akhil","Rohan","Golu"]
+choice= random.choice(lst)
+print(choice)
+
+
+#Read more on Random Modules 
