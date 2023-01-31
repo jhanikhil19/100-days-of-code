09fdc2860e96a95a929f1f40d83f7a42380e7a4a
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-01 04:59:11 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,15 @@
+# import sklearn as sk
+# print(sk.)
+
+
+import sys
+print(sys.path)
+
+# from sklearn.ensemble import RandomForestCassifier
+# print(RandomForestCassifier())
+
+import file2
+print(file2.a)
+
+from file2 import b
+print(b)
