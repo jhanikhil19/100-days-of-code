5669be111919a4afa7a722de4913ebe76f82745f
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-05 05:57:59 +0530

    Create tutmain1.py

---
+++ tutmain1.py
@@ -0,0 +1,13 @@
+
+
+def printhar(string):
+    return f"Ye string faaltu me hai {string}"
+
+def add(num1, num2):
+    return num1 + num2 + 5
+
+print("and the name is ",__name__)
+if __name__ == '__main__':
+    print(printhar("harry1"))
+    o=add(4,6)
+    print(o)
